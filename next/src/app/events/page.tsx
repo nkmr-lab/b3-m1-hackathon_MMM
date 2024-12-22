6dbaa94434e45563c7ac83e46e0d6c61922f60c9
non2d <onedanijo11@gmail.com>
2024-12-22 17:39:58 +0900

    Add "use client" directive and implement data fetching in Event component

--- next/src/app/events/page.tsx
+++ next/src/app/events/page.tsx
@@ -1,13 +1,34 @@
-/** 
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+/**
  * このコンポーネントはイベント紹介ページを表示します。
  * 
- * @returns {JSX.Element} 投稿一覧ページのJSX要素
+ * @returns {JSX.Element} イベント紹介ページのJSX要素
  */
-
 export default function Event() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/kaiji')
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
+
+    let content;
+
+    if (!data) { //データがまだロード(フェッチ)されていない場合
+        content = <p>Loading...</p>;
+    } else {
+        content = <pre>{JSON.stringify(data, null, 2)}</pre>;
+    }
+
     return (
         <>
-            イベント紹介ページです．
+            <h1>イベント紹介ページです．</h1>
+            {content}
         </>
     );
 }
