283afa2fe489d91a1850c15e3e72eec546126e70
NiijimaMichihiro-0408 <michimaru007.com@gmail.com>
2025-02-12 05:55:44 +0900

    あ

--- fastapi/app/routers/micchi.py
+++ fastapi/app/routers/micchi.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import select
 from db import get_db
 from pydantic import BaseModel
+import math
 
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -13,15 +14,22 @@ client=OpenAI()
 # routers
 router = APIRouter()
 
-@router.get("/micchi")
-async def test():
-    return "micchi"
-
-# db models
-from db import Base
-from sqlalchemy import Column, Integer, String
-
-class Micchi(Base):
-    __tablename__ = "micchi"
-    id = Column(Integer, primary_key=True, index=True)
-    micchi = Column(String(255), index=True)
+@router.get("/judge-spot")
+async def judge_spot(lat: float, lng: float):
+    # 複数のスポットをリストにして登録
+    spots = [
+        {"name": "明治大学", "lat": 10, "lng": 10},
+        {"name": "セントラルパーク", "lat": 20, "lng": 20},
+        {"name": "帝京平成大学", "lat": 30, "lng": 30},
+    ]
+    
+    spot_range = 10 
+    
+    
+    for spot in spots:
+        distance = math.sqrt((spot["lat"] - lat) ** 2 + (spot["lng"] - lng) ** 2)
+        if distance <= spot_range:
+            return spot["name"]
+    
+    # どのスポットとも10以上ならFalseを返す
+    return "該当なし"
